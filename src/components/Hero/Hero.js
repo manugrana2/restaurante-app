0e805aec2e4292fe7fa56f26b5575987fd95b7ca
manugrana2 <manugrana2@gmail.com>
2023-06-07 15:49:19 -0500

    Just another commit

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,22 @@
 export default function Hero() {
     return (
-        <section>
-            <h2>This is the hero section</h2>
+        <section className="hero">
+            <div className="left">
+                <div className="brand">
+                    <h1>Little Lemon <span className="hide">Restaurant</span></h1>
+                    <h2>Chicago</h2>
+                </div>
+                <div className="info">
+                    We are a family owned
+                    Mediterranean restaurant,
+                    focused on traditional
+                    recipes served with a modern
+                    twist.
+                </div>
+            </div>
+            <div className="right">
+                
+            </div>
         </section>
     )
 }
