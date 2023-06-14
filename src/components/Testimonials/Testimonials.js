f18cf191b99b204dc70b0308582c9747524219c2
manugrana2 <manugrana2@gmail.com>
2023-06-13 21:59:43 -0500

    Added a new testimonial

--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -19,6 +19,12 @@ export default function Testimonials() {
             name: 'Richard Roe',
             photo: 'https://th.bing.com/th/id/OIG.Tj4AEQ.xFpW8Ml6uLq1X?pid=ImgGn',
             review: 'The Little Lemon offers an incredible blend of traditional and modern Mediterranean cuisine. As a family-owned establishment in Chicago, they offer an inviting atmosphere that compliments their delicious dishes. An exceptional place to savor Mediterranean flavors!'
+        },
+        {
+            rating: 4,
+            name: 'Jennifer Smith',
+            photo: 'https://th.bing.com/th/id/OIG.79M6A0UFcfyMvwb1SaNM?w=270&h=270&c=6&r=0&o=5&dpr=1.3&pid=ImgGn',
+            review: 'I recently dined at The Little Lemon and had an amazing experience! The restaurant offers a delightful fusion of Mediterranean flavors that left me craving for more. The ambiance was cozy and welcoming, making it a perfect spot for a romantic dinner. The staff was friendly and attentive, ensuring that every detail of the service was top-notch. I highly recommend The Little Lemon to anyone looking for a memorable Mediterranean dining experience!'
         }
     ];
     return (
